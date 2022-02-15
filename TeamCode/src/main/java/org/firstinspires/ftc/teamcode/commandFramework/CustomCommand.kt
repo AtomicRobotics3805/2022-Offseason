8637eac701deb263936e64ada12402c14849f8fa
REMCodes <rowan.mcalpin@gmail.com>
2022-02-15 15:21:19 -0800

    Merged TimedCustomCommand and CustomCommand into a single class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandFramework/CustomCommand.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandFramework/CustomCommand.kt
@@ -1,4 +1,29 @@
 package org.firstinspires.ftc.teamcode.commandFramework
 
-class CustomCommand {
+import com.qualcomm.robotcore.util.ElapsedTime
+
+open class CustomCommand(
+    private val getDone: () -> Boolean = { true },
+    private val _execute: () -> Unit = { },
+    private val _start: () -> Unit = { },
+    private val _done: (interrupted: Boolean) -> Unit = { },
+    private val endTime: Double = -1.0
+): Command() {
+    private val timer = ElapsedTime()
+
+    override val _isDone: Boolean
+        get() = getDone.invoke() && (timer.seconds() > endTime || endTime >= 0)
+
+    override fun start() {
+        timer.reset()
+        _start.invoke()
+    }
+
+    override fun execute() {
+        _execute.invoke()
+    }
+
+    override fun end(interrupted: Boolean) {
+        _done.invoke(interrupted)
+    }
 }
