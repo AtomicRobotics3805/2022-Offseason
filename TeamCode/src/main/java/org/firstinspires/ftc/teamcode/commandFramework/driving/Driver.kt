def431ce69c7f2c871600a4b74eae9f283a3cbf4
Adam Meilicke <meiliada000@edmonds15.org>
2022-02-22 15:08:33 -0800

    Added driving commands & MotorToPosition

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandFramework/driving/Driver.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandFramework/driving/Driver.kt
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.commandFramework
 
+import com.acmerobotics.roadrunner.control.PIDFController
+import com.acmerobotics.roadrunner.drive.DriveSignal
 import com.acmerobotics.roadrunner.followers.TrajectoryFollower
+import com.acmerobotics.roadrunner.geometry.Pose2d
 import org.firstinspires.ftc.teamcode.commandFramework.trajectories.ParallelTrajectory
 
 /**
@@ -8,7 +11,12 @@ import org.firstinspires.ftc.teamcode.commandFramework.trajectories.ParallelTraj
  * DisplacementDelay. Drivetrain objects/classes like MecanumDrive should implement this interface.
  */
 interface Driver {
-
     var follower: TrajectoryFollower
     var trajectory: ParallelTrajectory?
+    var poseEstimate: Pose2d
+    var turnController: PIDFController
+    var rawExternalHeading: Double
+    var driverSpeed: Double
+    fun setDriveSignal(driveSignal: DriveSignal)
+    fun setWeightedDrivePower(drivePower: Pose2d)
 }
