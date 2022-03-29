445f967d276fda7e6133d313d42157a4828d9531
AdamMeilicke <meiliada000@edmonds15.org>
2022-03-29 15:09:47 -0700

    Added ExampleControls and ExampleTrajectoryFactory files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandFramework/example/other/ExampleTrajectoryFactory.kt
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.commandFramework.example.other
+
+import com.acmerobotics.dashboard.config.Config
+
+/**
+ * This object controls RoadRunner trajectories. These trajectories should be created and tested
+ * in the RRPathVisualizer project, then copied over to here. It also manages start positions for
+ * the robot, since those are inherently tied to the trajectories. It's empty since this is just a
+ * template project.
+ */
+@Suppress("unused", "MemberVisibilityCanBePrivate")
+@Config
+object ExampleTrajectoryFactory {
+
+    /**
+     * Initializes the start positions and the trajectories. That includes setting the actual
+     * coordinates and path segments. This should all be done in RRPathVisualizer first, then copied
+     * over here.
+     */
+    fun initialize() {
+
+    }
+}
