dfd5227b5f3a5b1478c85c96b174fbec0dd5e710
REM-Codes <rowan.mcalpin@gmail.com>
2022-04-26 15:14:35 -0700

    Moved Localizer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandFramework/subsystems/Localizer.kt
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode.commandFramework.subsystems
-
-import com.acmerobotics.roadrunner.localization.Localizer
-
-interface Localizer : Localizer, Subsystem {
-
-    override fun periodic() { update() }
-}
