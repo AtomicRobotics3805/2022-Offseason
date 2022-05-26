12a46b7786039f3037c436a8fc5b9b0ce30738ab
AdamMeilicke <meiliada000@edmonds15.org>
2022-05-26 14:28:46 -0700

    Finished DistanceSensorLocalizer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandFramework/driving/localizers/DistanceSensorLocalizer.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandFramework/driving/localizers/DistanceSensorLocalizer.kt
@@ -6,8 +6,17 @@ import com.acmerobotics.dashboard.config.Config
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.qualcomm.robotcore.hardware.AnalogInput
 import org.firstinspires.ftc.teamcode.commandFramework.Constants
+import org.firstinspires.ftc.teamcode.commandFramework.Constants.drive
 import org.firstinspires.ftc.teamcode.commandFramework.subsystems.Subsystem
+import org.firstinspires.ftc.teamcode.commandFramework.trajectories.toRadians
+import kotlin.math.cos
 
+/**
+ * Note: this localizer only works when you know that the forward distance sensor will be hitting
+ * the forward wall and the side sensor will be hitting the side wall. So basically, it only works
+ * if the robot isn't rotating enough for one of the distance sensors to hit a different wall. This
+ * is relevant in certain games like Freight Frenzy, but not in others.
+ */
 @Suppress("unused", "MemberVisibilityCanBePrivate")
 @Config
 class DistanceSensorLocalizer(
@@ -15,11 +24,15 @@ class DistanceSensorLocalizer(
 ) : Localizer {
 
     override var poseEstimate: Pose2d
-        get() = TODO("Not yet implemented")
-        set(value) {}
-    override val poseVelocity: Pose2d?
-        get() = TODO("Not yet implemented")
-
+        get() {
+            val x = sideDistance * cos(360.0.toRadians - drive.rawExternalHeading) - 72.0
+            val y = if (Constants.color == Constants.Color.BLUE)
+                forwardDistance * cos(360.0.toRadians - drive.rawExternalHeading) - 72.0
+            else -forwardDistance * cos(360.0.toRadians - drive.rawExternalHeading) + 144.0
+            return Pose2d(x, y, drive.rawExternalHeading)
+        }
+        set(value) { }
+    override val poseVelocity: Pose2d? = null
 
     private lateinit var forwardDistanceSensor: AnalogInput
     private lateinit var sideDistanceSensor: AnalogInput
@@ -41,7 +54,5 @@ class DistanceSensorLocalizer(
 interface DistanceSensorLocalizerConstants {
     val FORWARD_SENSOR_NAME: String
     val SIDE_SENSOR_NAME: String
-    val FORWARD_SENSOR_REVERSED: Boolean
-    val SIDE_SENSOR_REVERSED: Boolean
     val VOLTAGE_TO_DISTANCE: (Double) -> Double
 }
